c42a6ed066a35b94cb1c3863af92dbc3712f6a9c
Diquanbrugman <54799201+Diquanbrugman@users.noreply.github.com>
2020-01-12 13:00:35 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,23 @@
+let name = 'Diquan';
+console.log(name);
+
+let firstName = 'Diquan';
+let lastname = 'Brugman';
+let age = 19;
+let isApproved = true;
+let person = {
+    name: 'Diquan',
+    age: 19
+};
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors.length);
+
+function greet(name) {
+    console.log('Hello' + name);
+}
+ 
+greet('Diquan');
